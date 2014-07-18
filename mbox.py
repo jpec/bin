6150d7f5ffcf195557a31b205eb8caced543afc4
Julien Pecqueur <jpec@julienpecqueur.net>
2014-07-18 11:50:03 +0200

    Add mbox.py
    This tool list unread mails from the specified mailbox (mbox format).

---
+++ mbox.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import mailbox
+import os
+import sys
+
+def open_mbox(path):
+    if os.path.isfile("/var/mail/"+path):
+        mbox = mailbox.mbox("/var/mail/"+path)
+        mbox.lock()
+        return(mbox)
+    else:
+        return(None)
+
+def close_mbox(mbox):
+    mbox.flush()
+    mbox.unlock()
+    mbox.close()
+    return(True)
+
+def treat_message(message):
+    flags = message.get_flags()
+    if flags == "" or flags == "O":
+        return(message['date'] + " - " + message['subject'])
+    else:
+        return(None)
+
+def reading_messages(mbox):
+    unreads = list()
+    for key in mbox.keys():
+        unread = treat_message(mbox.get(key))
+        if unread:
+            unreads.append(unread)
+    return(unreads)
+
+def main(args):
+    if len(args) == 2:
+        mbox = open_mbox(args[1])
+        if mbox:
+            unreads = reading_messages(mbox)
+            close_mbox(mbox)
+            print("Nb unread mail(s):", len(unreads))
+            for message in unreads:
+                print(">", message)
+        else:
+            print("Oops. Cannot open the mailbox", args[1])
+    else:
+        print("Usage:", args[0], "mailbox")
+
+
+main(sys.argv)
+# EOF
