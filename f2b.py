bbbfc71db56b56809785a7a2d835cc9ffded6b44
Julien Pecqueur <jpec@julienpecqueur.net>
2014-10-29 17:07:45 +0100

    Update f2b.py

--- f2b.py
+++ f2b.py
@@ -20,7 +20,7 @@ def close_mbox(mbox):
 
 def treat_message(message):
     if message['from'] == 'Fail2Ban <fail2ban@peclu.net>':
-        return(message['date'] + " - " + message['subject'])
+        return(message['date'] + " - " + message['subject'].replace("[Fail2Ban] ssh: banned ", ""))
     else:
         return(None)
 
@@ -29,6 +29,7 @@ def reading_messages(mbox):
     for key in mbox.keys():
         unread = treat_message(mbox.get(key))
         if unread:
+            mbox.remove(key)
             f2b_messages.append(unread)
     return(f2b_messages)
 
@@ -38,9 +39,8 @@ def main(args):
         if mbox:
             f2b_messages = reading_messages(mbox)
             close_mbox(mbox)
-            print("Nb of ban(s):", len(f2b_messages))
             for message in f2b_messages:
-                print(">", message)
+                print(message)
         else:
             print("Oops. Cannot open the mailbox", args[1])
     else:
